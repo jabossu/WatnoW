3463dae2b42045e77fd1049a9933ec57280fbeda
Jacques-Alexandre Bossu <jacquesalexandrebossu@gmail.com>
2018-11-26 08:58:08 +0200

    Created the card system

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :cards
+  
   get 'about' => 'pages#about'
+  get 'cards' => 'cards/cards'
+
   root 'pages#home' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
