296e9eaaa03e3c9eb01c13c0cec5578d8a2f3172
Jacques-Alexandre Bossu <jacquesalexandrebossu@gmail.com>
2018-11-27 12:28:31 +0200

    Homepage now shows a random card

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @cards = Card.all
   end
 
   def about
   end
+
+  def allcards
+    @cards = Card.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'pages/allcards'
   resources :cards
   
   get 'about' => 'pages#about'
   get 'cards' => 'cards/cards'
+  get 'allcards' => 'pages#allcards'
 
   root 'pages#home' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
