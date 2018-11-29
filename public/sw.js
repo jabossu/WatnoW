4523c3f803e40898b07099fb82d1dc80c30c4c67
Jacques-Alexandre Bossu <jacquesalexandrebossu@gmail.com>
2018-11-29 19:16:42 +0200

    tweaked service worker

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 var CACHE_NAME = 'WatnoW-v1';
 var urlsToCache = [
-  '/about'
+  '/logout'
 ];
 
 self.addEventListener('install', function(event) {
@@ -11,5 +11,18 @@ self.addEventListener('install', function(event) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
